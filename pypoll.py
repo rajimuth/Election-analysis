5fb4d9d22fac47c1f8c6877cb9cadc21e7b5de8b
raji mk <raji.mukr@gmail.com>
2022-01-08 22:13:52 -0500

    Adding election-analysis

--- pypoll.py
+++ pypoll.py
@@ -12,6 +12,10 @@ candidate_options =[]
 # Open the election results and read the file.
 # Declare empty dictionary to store votes for each candidate
 candidate_votes ={}
+# Tracking the winning candidate, vote count, and percentage.
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 with open(election_results) as election_data:
     # Read the file object with the reader function
@@ -26,64 +30,81 @@ with open(election_results) as election_data:
         # Add the total Vote Count
         total_votes += 1
 
-        # to print the candidate name from each row
+        # get the candidate name from each row
         candidate_name = row[2]
-
-        
 
         # If the candidate does not match any existing candidate then add it to list cand-options
         if candidate_name not in candidate_options:
 
             # Add the candidate name to the candidate list
             candidate_options.append(candidate_name)
+
             # Begin tracking that candidate's vote count
             candidate_votes[candidate_name] = 0
                  
-            # Increament candidate vote count
+        # Increament candidate vote count
         candidate_votes[candidate_name] += 1
-    winning_candidate = ""
-    winning_count = 0
-    winning_percentage = 0
-    # Iterate through Candidate list
-    for candidate_name in candidate_votes:
-        
+
+    with open(election_write, "w") as txt_file:
+        election_results =(
+            f"\nElection Results\n"
+            f"--------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"-----------------------------\n")
+        print (election_results, end="")
+        txt_file.write(election_results)
+        # outfile = open(election_write, "w")
+        # outfile.write ('Election Results\n-------------------------------------\nTotal Votes: 369,711\n-----------------------------------------------------\n')
+            
+    
+        # Iterate through Candidate list
+        for candidate_name in candidate_votes:
+            
             # retrieve vote count of a candidate
-        votes = candidate_votes[candidate_name]
-        # Calculate the percentage of votes
-        Vote_percentage = float (votes) / float(total_votes)*100 
+            votes = candidate_votes[candidate_name]
+
+            # Calculate the percentage of votes
+            Vote_percentage = float (votes) / float(total_votes)*100 
+
             # print the candidate name and percentage of votes
-        print (f'{candidate_name}: received {Vote_percentage:.1f}% of the vote.')
-        # print out each candidate's name, vote count, and percentage of votes to terminal
-        print (f'{candidate_name} : {Vote_percentage:.1f}% ({votes:,})\n')
+            print (f'{candidate_name}: received {Vote_percentage:.1f}% of the vote.')
 
-        # Winning Candidate and Winning Count Tracker
-        
-        # Determine winning vote count and candidate
-        # Determine if the votes are greater than the winning count
-        if (votes > winning_count) and (Vote_percentage > winning_percentage):
-            # if the above condition is then set winning_count = votes and winning_percent = vote_percentage
-          winning_count = votes
-          winning_percentage = Vote_percentage
-          # set the winning_candidate equal to the candidate's name
-          winning_candidate = candidate_name
-    Winning_candidate_summary = (
-        f'.....................................\n'
-        f'Winner:{winning_candidate}\n'
-        f'Winning Vote Count: {winning_count:,}\n'
-        f'Winning Percentage: {winning_percentage:.1f}%\n'
-        f'--------------------------------------\n')
-    print (Winning_candidate_summary)
+            # print out each candidate's name, vote count, and percentage of votes to terminal
+            candidate_results = (f'{candidate_name} : {Vote_percentage:.1f}% ({votes:,})\n')
+            print (candidate_results)
+            txt_file.write(candidate_results)
+
+
+            # Winning Candidate and Winning Count Tracker        
+            # Determine winning vote count and candidate
+            # Determine if the votes are greater than the winning count
+            if (votes > winning_count) and (Vote_percentage > winning_percentage):
+                # if the above condition is then set winning_count = votes and winning_percent = vote_percentage
+                winning_count = votes
+                winning_percentage = Vote_percentage
+                # set the winning_candidate equal to the candidate's name
+                winning_candidate = candidate_name
+        #   Print the winning candidate's results to the terminal
+        Winning_candidate_summary = (
+            f'.....................................\n'
+            f'Winner:{winning_candidate}\n'
+            f'Winning Vote Count: {winning_count:,}\n'
+            f'Winning Percentage: {winning_percentage:.1f}%\n'
+            f'--------------------------------------\n')
+        print (Winning_candidate_summary)
+        # Save the winning candidate's name to the text file
+        txt_file.write (Winning_candidate_summary)
 
         
-    print (f'The winning candidate of the election is {winning_candidate} with {winning_count} votes and {winning_percentage:.1f}%')   
+#     print (f'The winning candidate of the election is {winning_candidate} with {winning_count} votes and {winning_percentage:.1f}%')   
 
      
-# Print the total votes
-print (f'Total votes casted = {total_votes}')
-# print the candidate list
-print(f'The candidates in election are {candidate_options}')
-#  Print each candidate votes
-print(f'The list of candidates and their vote is {candidate_votes}')
+# # Print the total votes
+# print (f'Total votes casted = {total_votes}')
+# # print the candidate list
+# print(f'The candidates in election are {candidate_options}')
+# #  Print each candidate votes
+# print(f'The list of candidates and their vote is {candidate_votes}')
 
     
 
